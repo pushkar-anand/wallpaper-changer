604689a1b372b27789c61e54f24023d9c84571b8
Pushkar Anand <pushkar-anand@users.noreply.github.com>
2017-10-21 16:21:53 +0530

    Create wallpaper.cpp

---
+++ wallpaper.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <unistd.h>
+#include <experimental/filesystem>
+
+using namespace std;
+int wallpaper_count_start=0;
+int main()
+{
+	vector<experimental::filesystem::directory_entry> pics;
+	int no_of_wallpapers;
+	int time_interval = 5; //time in minutes
+
+	string wallpaperDIR = "X/XX/XX/Wallpapers"; //the directory where wallpapers are stored
+
+	for (auto & p : experimental::filesystem::directory_iterator(wallpaperDIR))
+	{
+        	pics.push_back(p);
+	}
+	
+	no_of_wallpapers = pics.size();
+	string fileSTRING = pics[wallpaper_count_start].path().string();
+	
+	string cmd = "gsettings set org.gnome.desktop.background picture-uri "+ fileSTRING;
+	
+	system(cmd.c_str());
+	if(wallpaper_count_start==(no_of_wallpapers-1))
+	{
+		wallpaper_count_start=0;
+	}
+	else
+	{
+		wallpaper_count_start+=1;
+	}
+
+	sleep(time_interval*60);
+	main();
+	
+	/*for(auto it=pics.begin();it!=pics.end();it++)
+		cout<<*it;*/
+
+	return 0;
+}
