0cf3b2ef5098ff1a415230b2f165389097e95b38
Pushkar Anand <anandpushkar088@gmail.com>
2020-02-19 18:42:00 +0530

    Fixed issue with random wallpaper selection.

--- wallpaper.cpp
+++ wallpaper.cpp
@@ -8,7 +8,7 @@ int main()
 {
 	vector<experimental::filesystem::directory_entry> pics;
 	int no_of_wallpapers;
-	int time_interval = 2; //time in minutes
+	int time_interval = 1; //time in minutes
 
 	string wallpaperDIR = "/home/pushkar/Wallpapers"; //the directory where wallpapers are stored
 
@@ -20,11 +20,19 @@ int main()
 	no_of_wallpapers = pics.size();
 	
 	// random selection of wallpaper
-	wallpaper_count_start = 0 + (rand() % static_cast<int>(no_of_wallpapers - 0 + 1));
+	if(wallpaper_count_start == 0) {
+		cout<<"Random selection on start"<<endl;
+		srand(time(NULL));
+		wallpaper_count_start = 0 + (rand() % static_cast<int>(no_of_wallpapers - 0 + 1));
+	}
+	
+	cout<<"Selected Wallpaper count: "<<wallpaper_count_start<<endl;
+	
 	
 	string fileSTRING = pics[wallpaper_count_start].path().string();
 	
 	string cmd = "gsettings set org.gnome.desktop.background picture-uri \""+ fileSTRING +"\"";
+	cout<<"Issuing cmd: "<<cmd<<endl;
 	
 	system(cmd.c_str());
 	if(wallpaper_count_start== no_of_wallpapers-1 )
